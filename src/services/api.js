f5392740fe7bcf4c0aa5246b8678393702c60016
Lucas García Viz <lucas94gv@gmail.com>
2026-02-25 23:40:47 +0100

    add public routes and global errors

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// Backend
 const API = axios.create({
   baseURL: "http://localhost:3000/api/v1",
   headers: {
@@ -8,13 +7,28 @@ const API = axios.create({
   },
 });
 
-// Token
+// token
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
+
+// Global errors
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/";
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default API;
